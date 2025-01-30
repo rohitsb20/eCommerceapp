994c98085485b218f26b6614ec4150c885ed5302
rohit <rohitsb96134@gmail.com>
2025-01-30 22:03:32 +0530

    added hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,35 +27,43 @@ const NavLink = [
 
 const Navbar = () => {
   return (
-    <div
-      className=" p-2 flex  flex-col 
-    md:flex-row md:justify-around 
-     items-center justify-center rounded-lg"
-    >
-      <div className="text-xs md:text-sm font-medium flex items-center gap-x-1">
-        <AlignRight /> <span>Shop by categories</span>
-      </div>
+    <>
+    <div className="flex items-center justify-between  p-2 ">
 
-      {/* LINKS  */}
-      <div className="flex gap-x-4 items-center">
-        {NavLink.map((link) => (
-          <Link
-            className="
-                 text-xs md:text-sm 
+      <div
+        className=" p-2 flex  flex-col 
+      md:flex-row md:justify-around 
+      items-center justify-center rounded-lg w-[90%] md:w-full"
+      >
+        <div className="text-lg md:text-sm font-medium flex items-center gap-x-1">
+          <AlignRight /> <span>Shop by categories</span>
+        </div>
+
+        {/* LINKS  */}
+        <div className="hidden md:flex gap-x-4 items-center ">
+          {NavLink.map((link) => (
+              <Link
+              className="
+            text-xs md:text-sm 
                  font-medium
-                capitalize hover:scale-110 "
-            key={link.title}
-            to={link.href}
-          >
-            {link.title}
-          </Link>
-        ))}
+                 capitalize hover:scale-110 "
+              key={link.title}
+              to={link.href}
+              >
+              {link.title}
+            </Link>
+          ))}
+        </div>
       </div>
-      <div>
+      <div className="md:hidden w-[10%]">
         <Hamburger />
       </div>
-    </div>
+                </div>
+      
+    </>
   );
 }
+
+ 
 
 export default Navbar
